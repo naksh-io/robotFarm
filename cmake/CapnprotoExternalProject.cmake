27a2d38b82d24a1165300f11d1f773b8914ebcff
Anurag Jakhotia <5448528+ajakhotia@users.noreply.github.com>
2020-01-29 01:13:16 +0530

    add capnproto

---
+++ cmake/CapnprotoExternalProject.cmake
@@ -0,0 +1,20 @@
+#[[ Cmake guard. ]]
+if(TARGET CapnprotoExternalProject)
+    return()
+endif()
+
+include(ExternalProject)
+
+set(ROBOT_FARM_CAPNPROTO_URL
+    "https://capnproto.org/capnproto-c++-0.7.0.tar.gz"
+    CACHE STRING
+    "URL of the Cap'n'proto source archive")
+
+externalproject_add(CapnprotoExternalProject
+                    PREFIX ${CMAKE_CURRENT_BINARY_DIR}/capnproto
+                    URL ${ROBOT_FARM_CAPNPROTO_URL}
+                    DOWNLOAD_NO_PROGRESS ON
+                    CONFIGURE_COMMAND
+                        <SOURCE_DIR>/configure
+                            --prefix=${CMAKE_INSTALL_PREFIX}
+                            --with-sysroot=${CMAKE_INSTALL_PREFIX})
