8bf329ab882193e5ab05adf16413a872d98a9f3c
Darren Harton <dharton@carnegierobotics.com>
2020-04-16 17:15:46 -0400

    set oatpp dependency on oatpp-websocket

--- cmake/OatppWebSocketExternalProject.cmake
+++ cmake/OatppWebSocketExternalProject.cmake
@@ -1,9 +1,10 @@
 #[[ CMake guard. ]]
-if(TARGET OatppExternalProject)
+if(TARGET OatppWebsocketExternalProject)
     return()
 endif()
 
 include(ExternalProject)
+include(${CMAKE_CURRENT_LIST_DIR}/OatppExternalProject.cmake)
 
 set(ROBOT_FARM_OATPP_WEBSOCKET_URL
         "https://github.com/oatpp/oatpp-websocket/archive/1.0.0.zip"
@@ -19,3 +20,5 @@ externalproject_add(OatppWebsocketExternalProject
         -DCMAKE_PREFIX_PATH:PATH=${CMAKE_INSTALL_PREFIX}
         -DCMAKE_INSTALL_PREFIX:PATH=${CMAKE_INSTALL_PREFIX}
         -DBUILD_SHARED_LIBS:BOOL=ON)
+
+add_dependencies(OatppWebsocketExternalProject OatppExternalProject)
