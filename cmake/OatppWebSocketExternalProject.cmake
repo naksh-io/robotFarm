2cccc9898b4bebc7e5ca56e86711324c694ff505
Darren Harton <dharton@carnegierobotics.com>
2020-04-16 14:44:19 -0400

    added Oatpp-Websocket

---
+++ cmake/OatppWebSocketExternalProject.cmake
@@ -0,0 +1,21 @@
+#[[ CMake guard. ]]
+if(TARGET OatppExternalProject)
+    return()
+endif()
+
+include(ExternalProject)
+
+set(ROBOT_FARM_OATPP_WEBSOCKET_URL
+        "https://github.com/oatpp/oatpp-websocket/archive/1.0.0.zip"
+        CACHE STRING
+        "URL of the oatpp source archive")
+
+externalproject_add(OatppWebsocketExternalProject
+        PREFIX ${CMAKE_CURRENT_BINARY_DIR}/oatpp
+        URL ${ROBOT_FARM_OATPP_WEBSOCKET_URL}
+        DOWNLOAD_NO_PROGRESS ON
+        CMAKE_ARGS
+        -DCMAKE_BUILD_TYPE:STRING=${CMAKE_BUILD_TYPE}
+        -DCMAKE_PREFIX_PATH:PATH=${CMAKE_INSTALL_PREFIX}
+        -DCMAKE_INSTALL_PREFIX:PATH=${CMAKE_INSTALL_PREFIX}
+        -DBUILD_SHARED_LIBS:BOOL=ON)
