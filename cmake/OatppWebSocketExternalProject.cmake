1defbbb9439a528f4f12c658db111397a55dc0a1
Darren Harton <dharton@carnegierobotics.com>
2020-04-20 09:35:51 -0400

    addressed comments

--- cmake/OatppWebSocketExternalProject.cmake
+++ cmake/OatppWebSocketExternalProject.cmake
@@ -9,10 +9,10 @@ include(${CMAKE_CURRENT_LIST_DIR}/OatppExternalProject.cmake)
 set(ROBOT_FARM_OATPP_WEBSOCKET_URL
         "https://github.com/oatpp/oatpp-websocket/archive/1.0.0.zip"
         CACHE STRING
-        "URL of the oatpp source archive")
+        "URL of the oatpp-websocket source archive")
 
 externalproject_add(OatppWebsocketExternalProject
-        PREFIX ${CMAKE_CURRENT_BINARY_DIR}/oatpp
+        PREFIX ${CMAKE_CURRENT_BINARY_DIR}/oatpp-websocket
         URL ${ROBOT_FARM_OATPP_WEBSOCKET_URL}
         DOWNLOAD_NO_PROGRESS ON
         CMAKE_ARGS
