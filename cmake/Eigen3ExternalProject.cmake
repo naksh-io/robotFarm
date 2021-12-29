bcbec46d9f4c1b7a9b96bf3b94899a96fc59fb8b
Anurag Jakhotia <5448528+ajakhotia@users.noreply.github.com>
2021-12-29 01:55:56 +0100

    fix(eigen3): add dependency to boost

--- cmake/Eigen3ExternalProject.cmake
+++ cmake/Eigen3ExternalProject.cmake
@@ -5,6 +5,8 @@ endif()
 
 include(ExternalProject)
 # TODO: Add lapack, blas and suitesparse dependency.
+include(${CMAKE_CURRENT_LIST_DIR}/BoostExternalProject.cmake)
+
 
 set(ROBOT_FARM_EIGEN3_URL
     "https://gitlab.com/libeigen/eigen/-/archive/3.3.7/eigen-3.3.7.zip"
@@ -19,3 +21,5 @@ externalproject_add(Eigen3ExternalProject
                     -DCMAKE_BUILD_TYPE:STRING=${CMAKE_BUILD_TYPE}
                     -DCMAKE_PREFIX_PATH:PATH=${CMAKE_INSTALL_PREFIX}
                     -DCMAKE_INSTALL_PREFIX:PATH=${CMAKE_INSTALL_PREFIX})
+
+add_dependencies(Eigen3ExternalProject BoostExternalProject)
