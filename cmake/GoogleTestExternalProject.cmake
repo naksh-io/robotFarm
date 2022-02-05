23696da142f221e70730ee3cd5338102abce4409
Anurag Jakhotia <5448528+ajakhotia@users.noreply.github.com>
2022-02-05 16:08:07 +0100

    chore(gtest): reformat file

--- cmake/GoogleTestExternalProject.cmake
+++ cmake/GoogleTestExternalProject.cmake
@@ -6,12 +6,12 @@ endif()
 include(ExternalProject)
 
 set(ROBOT_FARM_GOOGLE_TEST_URL
-    "https://github.com/google/googletest/archive/refs/tags/release-1.11.0.tar.gz"
-    CACHE STRING
-    "URL of the Google Test source archive")
+        "https://github.com/google/googletest/archive/refs/tags/release-1.11.0.tar.gz"
+        CACHE STRING
+        "URL of the Google Test source archive")
 
 externalproject_add(GoogleTestExternalProject
-                    PREFIX ${CMAKE_CURRENT_BINARY_DIR}/googleTest
-                    URL ${ROBOT_FARM_GOOGLE_TEST_URL}
-                    DOWNLOAD_NO_PROGRESS ON
-                    CMAKE_ARGS ${ROBOT_FARM_FORWARDED_CMAKE_ARGS})
+        PREFIX ${CMAKE_CURRENT_BINARY_DIR}/googleTest
+        URL ${ROBOT_FARM_GOOGLE_TEST_URL}
+        DOWNLOAD_NO_PROGRESS ON
+        CMAKE_ARGS ${ROBOT_FARM_FORWARDED_CMAKE_ARGS})
