81d42c7f15ac4ffa6346f2f793318b6cd7fa7782
Anurag Jakhotia <5448528+ajakhotia@users.noreply.github.com>
2022-02-05 16:08:31 +0100

    chore(nlohmannjson): reformat file

--- cmake/NlohmannJsonExternalProject.cmake
+++ cmake/NlohmannJsonExternalProject.cmake
@@ -6,12 +6,12 @@ endif()
 include(ExternalProject)
 
 set(ROBOT_FARM_NLOHMANN_JSON_URL
-    "https://github.com/nlohmann/json/archive/refs/tags/v3.10.4.tar.gz"
-    CACHE STRING
-    "URL of the nlohmann's json source archive")
+        "https://github.com/nlohmann/json/archive/refs/tags/v3.10.4.tar.gz"
+        CACHE STRING
+        "URL of the nlohmann's json source archive")
 
 externalproject_add(NlohmannJsonExternalProject
-                    PREFIX ${CMAKE_CURRENT_BINARY_DIR}/nlohmann-json
-                    URL ${ROBOT_FARM_NLOHMANN_JSON_URL}
-                    DOWNLOAD_NO_PROGRESS ON
-                    CMAKE_ARGS ${ROBOT_FARM_FORWARDED_CMAKE_ARGS})
+        PREFIX ${CMAKE_CURRENT_BINARY_DIR}/nlohmann-json
+        URL ${ROBOT_FARM_NLOHMANN_JSON_URL}
+        DOWNLOAD_NO_PROGRESS ON
+        CMAKE_ARGS ${ROBOT_FARM_FORWARDED_CMAKE_ARGS})
