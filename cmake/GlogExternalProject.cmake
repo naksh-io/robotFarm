b9a6fa3a53fa4ab98d4e10e36b43f9605281b767
Anurag Jakhotia <5448528+ajakhotia@users.noreply.github.com>
2022-02-05 16:05:04 +0100

    chore(glog): reformat file

--- cmake/GlogExternalProject.cmake
+++ cmake/GlogExternalProject.cmake
@@ -7,14 +7,15 @@ include(ExternalProject)
 include(${CMAKE_CURRENT_LIST_DIR}/GFlagsExternalProject.cmake)
 
 set(ROBOT_FARM_GLOG_URL
-    "https://github.com/google/glog/archive/refs/tags/v0.5.0.tar.gz"
-    CACHE STRING
-    "URL of the Google Log source archive")
+        "https://github.com/google/glog/archive/refs/tags/v0.5.0.tar.gz"
+        CACHE STRING
+        "URL of the Google Log source archive")
 
 externalproject_add(GlogExternalProject
-                    PREFIX ${CMAKE_CURRENT_BINARY_DIR}/glog
-                    URL ${ROBOT_FARM_GLOG_URL}
-                    DOWNLOAD_NO_PROGRESS ON
-                    CMAKE_ARGS ${ROBOT_FARM_FORWARDED_CMAKE_ARGS})
+        PREFIX ${CMAKE_CURRENT_BINARY_DIR}/glog
+        URL ${ROBOT_FARM_GLOG_URL}
+        DOWNLOAD_NO_PROGRESS ON
+        CMAKE_ARGS ${ROBOT_FARM_FORWARDED_CMAKE_ARGS})
 
-add_dependencies(GlogExternalProject GFlagsExternalProject)
+add_dependencies(GlogExternalProject
+        GFlagsExternalProject)
