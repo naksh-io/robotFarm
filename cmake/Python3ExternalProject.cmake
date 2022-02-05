0c6f3bc097278034e5e20071ea38fda43ea90627
Anurag Jakhotia <5448528+ajakhotia@users.noreply.github.com>
2022-02-05 16:12:10 +0100

    chore(python3): reformat file

--- cmake/Python3ExternalProject.cmake
+++ cmake/Python3ExternalProject.cmake
@@ -6,34 +6,33 @@ endif()
 include(ExternalProject)
 
 set(ROBOT_FARM_PYTHON3_URL
-    "https://github.com/python/cpython/archive/refs/tags/v3.10.1.tar.gz"
-    CACHE STRING
-    "URL of the Python3 source archive")
+        "https://github.com/python/cpython/archive/refs/tags/v3.10.1.tar.gz"
+        CACHE STRING
+        "URL of the Python3 source archive")
 
 if(NOT ROBOT_FARM_SKIP_PYTHON3)
     externalproject_add(Python3ExternalProject
-                        PREFIX ${CMAKE_CURRENT_BINARY_DIR}/python3
-                        URL ${ROBOT_FARM_PYTHON3_URL}
-                        DOWNLOAD_NO_PROGRESS ON
+            PREFIX ${CMAKE_CURRENT_BINARY_DIR}/python3
+            URL ${ROBOT_FARM_PYTHON3_URL}
+            DOWNLOAD_NO_PROGRESS ON
 
-                        CONFIGURE_COMMAND <SOURCE_DIR>/configure
-                        --prefix=${CMAKE_INSTALL_PREFIX}
-                        --enable-shared
-                        --enable-optimizations
-                        --enable-loadable-sqlite-extensions
-                        --disable-ipv6
-                        --enable-big-digits=30
-                        --with-lto
-                        --with-system-expat
-                        --with-system-ffi
-                        --with-system-libmpdec
-                        --with-signal-module
-                        --with-threads
-                        --with-pymalloc
-                        --with-fpectl
-                        --with-computed-gotos
-                        --with-ensurepip=upgrade
-                        )
+            CONFIGURE_COMMAND <SOURCE_DIR>/configure
+                --prefix=${CMAKE_INSTALL_PREFIX}
+                --enable-shared
+                --enable-optimizations
+                --enable-loadable-sqlite-extensions
+                --disable-ipv6
+                --enable-big-digits=30
+                --with-lto
+                --with-system-expat
+                --with-system-ffi
+                --with-system-libmpdec
+                --with-signal-module
+                --with-threads
+                --with-pymalloc
+                --with-fpectl
+                --with-computed-gotos
+                --with-ensurepip=upgrade)
 else()
     add_custom_target(Python3ExternalProject)
 endif()
