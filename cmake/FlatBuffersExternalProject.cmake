4f477ec2292f5998bf4a419b1fee26524444fa82
Anurag Jakhotia <5448528+ajakhotia@users.noreply.github.com>
2022-02-05 16:03:40 +0100

    chore(flatbuffers): reformat file

--- cmake/FlatBuffersExternalProject.cmake
+++ cmake/FlatBuffersExternalProject.cmake
@@ -6,13 +6,14 @@ endif()
 include(ExternalProject)
 
 set(ROBOT_FARM_FLAT_BUFFERS_URL
-    "https://github.com/google/flatbuffers/archive/refs/tags/v2.0.0.tar.gz"
-    CACHE STRING
-    "URL of the Flat Buffers source archive")
+        "https://github.com/google/flatbuffers/archive/refs/tags/v2.0.0.tar.gz"
+        CACHE STRING
+        "URL of the Flat Buffers source archive")
 
 externalproject_add(FlatBuffersExternalProject
-                    PREFIX ${CMAKE_CURRENT_BINARY_DIR}/flatbuffers
-                    URL ${ROBOT_FARM_FLAT_BUFFERS_URL}
-                    DOWNLOAD_NO_PROGRESS ON
-                    CMAKE_ARGS ${ROBOT_FARM_FORWARDED_CMAKE_ARGS}
-                    -DFLATBUFFERS_BUILD_SHAREDLIB:BOOL=${BUILD_SHARED_LIBS})
+        PREFIX ${CMAKE_CURRENT_BINARY_DIR}/flatbuffers
+        URL ${ROBOT_FARM_FLAT_BUFFERS_URL}
+        DOWNLOAD_NO_PROGRESS ON
+        CMAKE_ARGS
+            ${ROBOT_FARM_FORWARDED_CMAKE_ARGS}
+            -DFLATBUFFERS_BUILD_SHAREDLIB:BOOL=${BUILD_SHARED_LIBS})
