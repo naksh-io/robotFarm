3e17d9ddf2c26b2ad738ab9292936cce0417579b
Anurag Jakhotia <5448528+ajakhotia@users.noreply.github.com>
2022-02-05 16:02:13 +0100

    chore(eigen3): reformat file

--- cmake/Eigen3ExternalProject.cmake
+++ cmake/Eigen3ExternalProject.cmake
@@ -9,14 +9,15 @@ include(${CMAKE_CURRENT_LIST_DIR}/BoostExternalProject.cmake)
 
 
 set(ROBOT_FARM_EIGEN3_URL
-    "https://gitlab.com/libeigen/eigen/-/archive/3.4.0/eigen-3.4.0.tar.gz"
-    CACHE STRING
-    "URL of the Eigen3 source archive")
+        "https://gitlab.com/libeigen/eigen/-/archive/3.4.0/eigen-3.4.0.tar.gz"
+        CACHE STRING
+        "URL of the Eigen3 source archive")
 
 externalproject_add(Eigen3ExternalProject
-                    PREFIX ${CMAKE_CURRENT_BINARY_DIR}/eigen3
-                    URL ${ROBOT_FARM_EIGEN3_URL}
-                    DOWNLOAD_NO_PROGRESS ON
-                    CMAKE_ARGS ${ROBOT_FARM_FORWARDED_CMAKE_ARGS})
+        PREFIX ${CMAKE_CURRENT_BINARY_DIR}/eigen3
+        URL ${ROBOT_FARM_EIGEN3_URL}
+        DOWNLOAD_NO_PROGRESS ON
+        CMAKE_ARGS ${ROBOT_FARM_FORWARDED_CMAKE_ARGS})
 
-add_dependencies(Eigen3ExternalProject BoostExternalProject)
+add_dependencies(Eigen3ExternalProject
+        BoostExternalProject)
