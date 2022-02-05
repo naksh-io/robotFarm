a87838575fa5ce3aa6f87cc23ab30b94ee2664d1
Anurag Jakhotia <5448528+ajakhotia@users.noreply.github.com>
2022-02-05 16:04:35 +0100

    chore(glags): reformat file

--- cmake/GFlagsExternalProject.cmake
+++ cmake/GFlagsExternalProject.cmake
@@ -6,17 +6,18 @@ endif()
 include(ExternalProject)
 
 set(ROBOT_FARM_GFLAGS_URL
-    "https://github.com/gflags/gflags/archive/refs/tags/v2.2.2.tar.gz"
-    CACHE STRING
-    "URL of the Google Flags source archive")
+        "https://github.com/gflags/gflags/archive/refs/tags/v2.2.2.tar.gz"
+        CACHE STRING
+        "URL of the Google Flags source archive")
 
 externalproject_add(GFlagsExternalProject
-                    PREFIX ${CMAKE_CURRENT_BINARY_DIR}/gflags
-                    URL ${ROBOT_FARM_GFLAGS_URL}
-                    DOWNLOAD_NO_PROGRESS ON
-                    CMAKE_ARGS ${ROBOT_FARM_FORWARDED_CMAKE_ARGS}
-                        -DBUILD_STATIC_LIBS:BOOL=ON
-                        -DBUILD_gflags_LIB:BOOL=ON
-                        -DBUILD_gflags_nothreads_LIB:BOOL=ON
-                        -DINSTALL_SHARED_LIBS:BOOL=${BUILD_SHARED_LIBS}
-                        -DINSTALL_STATIC_LIBS:BOOL=ON)
+        PREFIX ${CMAKE_CURRENT_BINARY_DIR}/gflags
+        URL ${ROBOT_FARM_GFLAGS_URL}
+        DOWNLOAD_NO_PROGRESS ON
+        CMAKE_ARGS
+            ${ROBOT_FARM_FORWARDED_CMAKE_ARGS}
+            -DBUILD_STATIC_LIBS:BOOL=ON
+            -DBUILD_gflags_LIB:BOOL=ON
+            -DBUILD_gflags_nothreads_LIB:BOOL=ON
+            -DINSTALL_SHARED_LIBS:BOOL=${BUILD_SHARED_LIBS}
+            -DINSTALL_STATIC_LIBS:BOOL=ON)
