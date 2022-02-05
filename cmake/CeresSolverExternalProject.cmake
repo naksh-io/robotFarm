9e0ec6c2db71debcedb31918d124b3611f7badd8
Anurag Jakhotia <5448528+ajakhotia@users.noreply.github.com>
2022-02-05 16:01:43 +0100

    chore(ceres): reformat file

--- cmake/CeresSolverExternalProject.cmake
+++ cmake/CeresSolverExternalProject.cmake
@@ -9,15 +9,18 @@ include(${CMAKE_CURRENT_LIST_DIR}/GlogExternalProject.cmake)
 include(${CMAKE_CURRENT_LIST_DIR}/Eigen3ExternalProject.cmake)
 
 set(ROBOT_FARM_CERES_SOLVER_URL
-    "https://github.com/ceres-solver/ceres-solver/archive/refs/tags/2.0.0.tar.gz"
-    CACHE STRING
-    "URL of the Ceres Solver source archive")
+        "https://github.com/ceres-solver/ceres-solver/archive/refs/tags/2.0.0.tar.gz"
+        CACHE STRING
+        "URL of the Ceres Solver source archive")
 
 externalproject_add(CeresSolverExternalProject
-                    PREFIX ${CMAKE_CURRENT_BINARY_DIR}/ceressolver
-                    URL ${ROBOT_FARM_CERES_SOLVER_URL}
-                    DOWNLOAD_NO_PROGRESS ON
-                    CMAKE_ARGS ${ROBOT_FARM_FORWARDED_CMAKE_ARGS})
+        PREFIX ${CMAKE_CURRENT_BINARY_DIR}/ceressolver
+        URL ${ROBOT_FARM_CERES_SOLVER_URL}
+        DOWNLOAD_NO_PROGRESS ON
+        CMAKE_ARGS ${ROBOT_FARM_FORWARDED_CMAKE_ARGS})
 
 
-add_dependencies(CeresSolverExternalProject GFlagsExternalProject GlogExternalProject Eigen3ExternalProject)
+add_dependencies(CeresSolverExternalProject
+        GFlagsExternalProject
+        GlogExternalProject
+        Eigen3ExternalProject)
