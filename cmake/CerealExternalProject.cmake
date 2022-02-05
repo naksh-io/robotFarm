6a816ff035d6a9ca20318260f932c43959b248bf
Anurag Jakhotia <5448528+ajakhotia@users.noreply.github.com>
2022-02-05 16:01:09 +0100

    chore(cereal): reformat file

--- cmake/CerealExternalProject.cmake
+++ cmake/CerealExternalProject.cmake
@@ -6,12 +6,12 @@ endif()
 include(ExternalProject)
 
 set(ROBOT_FARM_CEREAL_URL
-    "https://github.com/USCiLab/cereal/archive/refs/tags/v1.3.0.tar.gz"
-    CACHE STRING
-    "URL of the Cereal source archive")
+        "https://github.com/USCiLab/cereal/archive/refs/tags/v1.3.0.tar.gz"
+        CACHE STRING
+        "URL of the Cereal source archive")
 
 externalproject_add(CerealExternalProject
-                    PREFIX ${CMAKE_CURRENT_BINARY_DIR}/cereal
-                    URL ${ROBOT_FARM_CEREAL_URL}
-                    DOWNLOAD_NO_PROGRESS ON
-                    CMAKE_ARGS ${ROBOT_FARM_FORWARDED_CMAKE_ARGS})
+        PREFIX ${CMAKE_CURRENT_BINARY_DIR}/cereal
+        URL ${ROBOT_FARM_CEREAL_URL}
+        DOWNLOAD_NO_PROGRESS ON
+        CMAKE_ARGS ${ROBOT_FARM_FORWARDED_CMAKE_ARGS})
