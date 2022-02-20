3a582f892a6ccdb07f805c2b12090c2eedc3d770
Anurag Jakhotia <5448528+ajakhotia@users.noreply.github.com>
2022-02-20 13:37:56 +0100

    feat(boost): add skip flags

--- cmake/BoostExternalProject.cmake
+++ cmake/BoostExternalProject.cmake
@@ -6,22 +6,27 @@ endif()
 include(ExternalProject)
 include(${CMAKE_CURRENT_LIST_DIR}/Python3ExternalProject.cmake)
 
-set(ROBOT_FARM_BOOST_URL
+option(ROBOT_FARM_SKIP_BOOST "Skip Boost" OFF)
+
+if(ROBOT_FARM_SKIP_BOOST)
+    add_custom_target(BoostExternalProject)
+else()
+    set(ROBOT_FARM_BOOST_URL
         "https://boostorg.jfrog.io/artifactory/main/release/1.78.0/source/boost_1_78_0.tar.gz"
-        CACHE STRING
-        "URL of the Boost source archive")
+        CACHE STRING "URL of the Boost source archive")
 
-# See: https://www.boost.org/doc/libs/1_78_0/more/getting_started/unix-variants.html#identify-your-toolset
-set(ROBOT_FARM_BOOST_TOOLSET gcc CACHE STRING "Toolset name to use for building boost[Default: gcc]")
+    # See: https://www.boost.org/doc/libs/1_78_0/more/getting_started/unix-variants.html#identify-your-toolset
+    set(ROBOT_FARM_BOOST_TOOLSET gcc CACHE
+        STRING "Toolset name to use for building boost[Default: gcc]")
 
-set(BOOST_LINK_TYPE static)
-if(BUILD_SHARED_LIBS)
-    set(BOOST_LINK_TYPE shared)
-endif()
+    set(BOOST_LINK_TYPE static)
+    if(BUILD_SHARED_LIBS)
+        set(BOOST_LINK_TYPE shared)
+    endif()
 
-string(TOLOWER ${CMAKE_BUILD_TYPE} BOOST_VARIANT_TYPE)
+    string(TOLOWER ${CMAKE_BUILD_TYPE} BOOST_VARIANT_TYPE)
 
-externalproject_add(BoostExternalProject
+    externalproject_add(BoostExternalProject
         PREFIX ${CMAKE_CURRENT_BINARY_DIR}/boost
         URL ${ROBOT_FARM_BOOST_URL}
         DOWNLOAD_NO_PROGRESS ON
@@ -30,22 +35,24 @@ externalproject_add(BoostExternalProject
             LD_LIBRARY_PATH=${CMAKE_INSTALL_PREFIX}/lib:$ENV{LD_LIBRARY_PATH}
             PATH=${CMAKE_INSTALL_PREFIX}/bin:$ENV{PATH}
             <SOURCE_DIR>/bootstrap.sh
-            --prefix=${CMAKE_INSTALL_PREFIX}
-            --with-libraries=all
-            --with-python-root=${CMAKE_INSTALL_PREFIX}
-            --with-python=python3
-            --with-toolset=${ROBOT_FARM_BOOST_TOOLSET}
+                --prefix=${CMAKE_INSTALL_PREFIX}
+                --with-libraries=all
+                --with-python-root=${CMAKE_INSTALL_PREFIX}
+                --with-python=python3
+                --with-toolset=${ROBOT_FARM_BOOST_TOOLSET}
 
-        BUILD_COMMAND <SOURCE_DIR>/b2
-            --build-type=complete
-            --layout=tagged
-            install
-            link=${BOOST_LINK_TYPE}
-            runtime-link=${BOOST_LINK_TYPE}
-            threading=multi
-            variant=${BOOST_VARIANT_TYPE}
+        BUILD_COMMAND
+            <SOURCE_DIR>/b2
+                --build-type=complete
+                --layout=tagged
+                install
+                link=${BOOST_LINK_TYPE}
+                runtime-link=${BOOST_LINK_TYPE}
+                threading=multi
+                variant=${BOOST_VARIANT_TYPE}
 
         BUILD_IN_SOURCE ON
         INSTALL_COMMAND "")
+endif()
 
 add_dependencies(BoostExternalProject Python3ExternalProject)
