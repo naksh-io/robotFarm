a67ca17963fc6b5f7b78c37b547ea00834ede614
Anurag Jakhotia <5448528+ajakhotia@users.noreply.github.com>
2022-02-20 16:05:56 +0100

    feat(oatpp+websocket): add skip flags

--- cmake/OatppExternalProject.cmake
+++ cmake/OatppExternalProject.cmake
@@ -5,13 +5,19 @@ endif()
 
 include(ExternalProject)
 
-set(ROBOT_FARM_OATPP_URL
+option(ROBOT_FARM_SKIP_OATPP "Skip Oatpp" OFF)
+
+if(ROBOT_FARM_SKIP_OATPP)
+    add_custom_target(OatppExternalProject)
+else()
+    set(ROBOT_FARM_OATPP_URL
         "https://github.com/oatpp/oatpp/archive/refs/tags/1.3.0.tar.gz"
         CACHE STRING
         "URL of the oatpp source archive")
 
-externalproject_add(OatppExternalProject
+    externalproject_add(OatppExternalProject
         PREFIX ${CMAKE_CURRENT_BINARY_DIR}/oatpp
         URL ${ROBOT_FARM_OATPP_URL}
         DOWNLOAD_NO_PROGRESS ON
         CMAKE_ARGS ${ROBOT_FARM_FORWARDED_CMAKE_ARGS})
+endif()
