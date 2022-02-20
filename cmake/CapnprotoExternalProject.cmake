1042642d1144edcecf1dba15092c7a8c55c200d3
Anurag Jakhotia <5448528+ajakhotia@users.noreply.github.com>
2022-02-20 13:50:10 +0100

    feat(capnproto): add skip flags

--- cmake/CapnprotoExternalProject.cmake
+++ cmake/CapnprotoExternalProject.cmake
@@ -5,13 +5,19 @@ endif()
 
 include(ExternalProject)
 
-set(ROBOT_FARM_CAPNPROTO_URL
+option(ROBOT_FARM_SKIP_CAPNPROTO "Skip Cap'n proto" OFF)
+
+if(ROBOT_FARM_SKIP_CAPNPROTO)
+    add_custom_target(CapnprotoExternalProject)
+else()
+    set(ROBOT_FARM_CAPNPROTO_URL
         "https://github.com/capnproto/capnproto/archive/refs/tags/v0.9.1.tar.gz"
         CACHE STRING
         "URL of the Cap'n'proto source archive")
 
-externalproject_add(CapnprotoExternalProject
+    externalproject_add(CapnprotoExternalProject
         PREFIX ${CMAKE_CURRENT_BINARY_DIR}/capnproto
         URL ${ROBOT_FARM_CAPNPROTO_URL}
         DOWNLOAD_NO_PROGRESS ON
         CMAKE_ARGS ${ROBOT_FARM_FORWARDED_CMAKE_ARGS})
+endif()
