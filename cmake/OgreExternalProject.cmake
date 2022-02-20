3429435f17a40803d5925ff7af3e579cace84350
Anurag Jakhotia <5448528+ajakhotia@users.noreply.github.com>
2022-02-20 16:10:22 +0100

    feat(ogre): add skip flags

--- cmake/OgreExternalProject.cmake
+++ cmake/OgreExternalProject.cmake
@@ -6,18 +6,25 @@ endif()
 include(ExternalProject)
 include(${CMAKE_CURRENT_LIST_DIR}/BoostExternalProject.cmake)
 
-set(ROBOT_FARM_OGRE_URL
+option(ROBOT_FARM_SKIP_OGRE "Skip Ogre" OFF)
+
+if(ROBOT_FARM_SKIP_OGRE)
+    add_custom_target(OgreExternalProject)
+else()
+    set(ROBOT_FARM_OGRE_URL
         "https://github.com/OGRECave/ogre/archive/refs/tags/v13.2.4.tar.gz"
         CACHE STRING
         "URL of the OGRE source archive")
 
-externalproject_add(OgreExternalProject
+    externalproject_add(OgreExternalProject
         PREFIX ${CMAKE_CURRENT_BINARY_DIR}/ogre
         URL ${ROBOT_FARM_OGRE_URL}
         DOWNLOAD_NO_PROGRESS ON
-        CMAKE_ARGS ${ROBOT_FARM_FORWARDED_CMAKE_ARGS}
-        -DOGRE_BUILD_COMPONENT_OVERLAY_IMGUI:BOOL=OFF
-        -DOGRE_BUILD_DEPENDENCIES:BOOL=OFF)
+        CMAKE_ARGS
+            ${ROBOT_FARM_FORWARDED_CMAKE_ARGS}
+            -DOGRE_BUILD_COMPONENT_OVERLAY_IMGUI:BOOL=OFF
+            -DOGRE_BUILD_DEPENDENCIES:BOOL=OFF)
+endif()
 
 add_dependencies(OgreExternalProject
-        BoostExternalProject)
+    BoostExternalProject)
