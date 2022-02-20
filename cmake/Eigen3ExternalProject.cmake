83121d261c737486a8b877d183984d6958b1a9d8
Anurag Jakhotia <5448528+ajakhotia@users.noreply.github.com>
2022-02-20 15:31:47 +0100

    feat(eigen3): add skip flags

--- cmake/Eigen3ExternalProject.cmake
+++ cmake/Eigen3ExternalProject.cmake
@@ -4,20 +4,24 @@ if(TARGET Eigen3ExternalProject)
 endif()
 
 include(ExternalProject)
-# TODO: Add lapack, blas and suitesparse dependency.
 include(${CMAKE_CURRENT_LIST_DIR}/BoostExternalProject.cmake)
 
+option(ROBOT_FARM_SKIP_EIGEN3 "Skip Eigen3" OFF)
 
-set(ROBOT_FARM_EIGEN3_URL
+if(ROBOT_FARM_SKIP_EIGEN3)
+    add_custom_target(Eigen3ExternalProject)
+else()
+    set(ROBOT_FARM_EIGEN3_URL
         "https://gitlab.com/libeigen/eigen/-/archive/3.4.0/eigen-3.4.0.tar.gz"
         CACHE STRING
         "URL of the Eigen3 source archive")
 
-externalproject_add(Eigen3ExternalProject
+    externalproject_add(Eigen3ExternalProject
         PREFIX ${CMAKE_CURRENT_BINARY_DIR}/eigen3
         URL ${ROBOT_FARM_EIGEN3_URL}
         DOWNLOAD_NO_PROGRESS ON
         CMAKE_ARGS ${ROBOT_FARM_FORWARDED_CMAKE_ARGS})
+endif()
 
 add_dependencies(Eigen3ExternalProject
-        BoostExternalProject)
+    BoostExternalProject)
