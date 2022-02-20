5e6a053d152e01f280b6b623df723aa3eb1a7289
Anurag Jakhotia <5448528+ajakhotia@users.noreply.github.com>
2022-02-20 15:46:50 +0100

    feat(glog): add skip flags

--- cmake/GlogExternalProject.cmake
+++ cmake/GlogExternalProject.cmake
@@ -6,16 +6,22 @@ endif()
 include(ExternalProject)
 include(${CMAKE_CURRENT_LIST_DIR}/GFlagsExternalProject.cmake)
 
-set(ROBOT_FARM_GLOG_URL
+option(ROBOT_FARM_SKIP_GLOG "Skip Glog" OFF)
+
+if(ROBOT_FARM_SKIP_GLOG)
+    add_custom_target(GlogExternalProject)
+else()
+    set(ROBOT_FARM_GLOG_URL
         "https://github.com/google/glog/archive/refs/tags/v0.5.0.tar.gz"
         CACHE STRING
         "URL of the Google Log source archive")
 
-externalproject_add(GlogExternalProject
+    externalproject_add(GlogExternalProject
         PREFIX ${CMAKE_CURRENT_BINARY_DIR}/glog
         URL ${ROBOT_FARM_GLOG_URL}
         DOWNLOAD_NO_PROGRESS ON
         CMAKE_ARGS ${ROBOT_FARM_FORWARDED_CMAKE_ARGS})
+endif()
 
 add_dependencies(GlogExternalProject
-        GFlagsExternalProject)
+    GFlagsExternalProject)
